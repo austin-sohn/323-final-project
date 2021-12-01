65540d4afcc0e6f18490252062fab8437849d02d
austin-sohn <sohn.austin@gmail.com>
2021-11-30 19:58:04 -0800

    updated 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -6,30 +6,46 @@
 using namespace std;
 
 // adds spaces before and after tokens
-string token(string s){
-    char token[8] = {',','=' ,';', ':','(',')',  '+', '*'};
-    string newstr;
-    int counter = 0;
-    for(int i = 0; i < 8; i++){
-        for(int j = 0; j < s.size(); j++){
-            //cout << s.size() << endl;
-            cout << s.at(j) << endl;
-            if(s.at(j) == '_')
-                
-            if(s.at(j) == '"')
-                ++counter;
-            if(s.at(j) == token[i] && counter != 1){
-                
-                //newstr = s.substr(0, s.find(token[i])) + "_" + token[i] + "_" + s.substr(s.find(token[i])+1, s.size());  // main one
-                newstr += s.substr(0, s.find(token[i])) + "_" + token[i];
-                s = s.substr(s.find(token[i])+1, s.size());
-                j = 0;
-            }
-            if(counter == 2)
-                counter = 0;
+string remove_space(string s){
+    string newstr = "";
+    for(int i = 0; i < s.size(); i++){
+        if(s[i] != ' '){
+            newstr.push_back(s[i]);
         }
     }
-    newstr += s;
+    return newstr;
+}
+string token(string s){
+    char token[8] = {',','=',';',':','(',')','+','*'};
+    int len = sizeof(token)/sizeof(token[0]);
+    string newstr, temp;
+    int counter = 0;
+    bool b = false;
+
+    for(int i = 0; i < s.size()+1; i++){
+        if(b == true){
+            newstr.push_back(' ');
+            b = false;
+        }
+        if(s[i] == '"'){
+            counter++;
+        }
+        for(int j = 0; j < len; j++){
+            if(s[i] == token[j] && counter!= 1){
+                b = true;
+                if(newstr[newstr.size()-1] == ' '){
+                    break;
+                }
+                else{
+                    newstr.push_back(' ');
+                }    
+            }
+        }
+        if(counter == 2){
+            counter = 0;
+        }
+        newstr.push_back(s[i]);
+    }
     return newstr;
 }
 
@@ -37,7 +53,6 @@ string token(string s){
 string comment(string s){
     int pos = s.find("**");
     string newstr;
-    //cout << s.size() - 2 << " " << pos << endl;
     if(pos == -1){
         return s;
     }
@@ -50,18 +65,26 @@ string comment(string s){
     return newstr;
 }
 
+bool check_error(){
+
+    return false;
+}
+
 int main(){
     string line;
     vector<string> v;
     ifstream finalp1 ("finalp1.txt");
     ofstream finalp2 ("finalp2.txt");
+
+    string res[6] = {"program", "var", "begin", "end.", "integer", "write"};
+    bool error = true;
+
     if (finalp1.is_open()){
         while (getline(finalp1,line)){
-            line.erase(remove(line.begin(), line.end(), ' '), line.end());
+            line = remove_space(line);
             line = comment(line);
-            if(!line.empty()){
+            if(line != ""){
                 line = token(line);
-                
                 v.push_back(line);
             }
         }
@@ -73,4 +96,20 @@ int main(){
         finalp2 << v.at(i) << endl;
     }
     finalp2.close();
+    error = check_error();
+    if(error == false){
+        cout << "#include <iostream>\n";
+        cout << "using namespace std;\n";
+        cout << "int main()\n";
+        cout << "{\n";
+        for(int i = 0; i < v.size(); i++){
+            cout << "\t" << v.at(i) << endl;
+        }
+        cout << "\treturn 0;\n";
+        cout << "}\n";
+    }
+    else{
+        cout << "There was an error\n";
+    }
+    
 }
