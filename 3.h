84a94a1d4c3502b4e0b1b907c242bf259fed6c7e
austin-sohn <sohn.austin@gmail.com>
2021-12-08 22:33:22 -0800

    updated to output the output

--- 3.h
+++ 3.h
@@ -10,6 +10,7 @@ void output(string s){
     vector<string> res = {"program", "var", "begin", "end.", "integer", "write"};
     vector<string> v;
     string read,reserve,tmp,tmpres;
+    ofstream o ("output.cpp");
     bool b;
         
     while(!s.empty()){
@@ -30,6 +31,7 @@ void output(string s){
 
         if(reserve == "program"){
             cout << "#include <iostream>\nusing namespace std;\nint main()\n{\n\t";
+            o << "#include <iostream>\nusing namespace std;\nint main()\n{\n\t";
             reserve = "";
         }
 
@@ -40,15 +42,19 @@ void output(string s){
             tmp = tmp.substr(0, tmp.find(":")) + ";\n\t";
             if(types == "integer"){
                 cout << "int ";
+                o << "int ";
             }
             cout << tmp;
+            o << tmp;
             read = read.substr(read.find(";"+1), read.length());
 
             tmpres = reserve;
         }
         else if(reserve == "begin"){
             tmpres = reserve;
+            
             cout << read << "\n\t";
+            o << read << "\n\t";
             read = s.substr(0, s.find(";")+1);
         }
         else if(reserve == "write"){
@@ -56,15 +62,21 @@ void output(string s){
             read = read.substr(0,read.find(")"));
             
             cout << "cout << " << read.substr(0,read.find(" "));
+            o << "cout << " << read.substr(0,read.find(" "));
             if(read.find(",")!= -1){
                 cout << " << " << read.substr(read.find(",")+2, read.length());
+                o << " << " << read.substr(read.find(",")+2, read.length());
             }
-            cout << ";\n\t";
+            cout << " << endl"";\n\t";
+            o << " << endl"";\n\t";
             reserve = tmpres;
         }
         else if(reserve == "end."){
             cout << "return 0;\n}";
+            o << "return 0;\n}";
             s = "";
         }
     }
+
+
 }
